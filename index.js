3bb994562180f7318703eb7fd0808964bdaf5873
Muhammad Musah <mmusah1@gmail.com>
2019-04-04 18:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function currentLine(katzDeli) {
    if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else{
-  return(`The line is currently:` + katzDeli);
+  return(`The line is currently:` + line);
   }
 }
